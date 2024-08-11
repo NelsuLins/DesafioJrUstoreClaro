1763b8f99852f6469cb58c0699bd7fe1e46f1686
Nelson Lins <54917418+NelsuLins@users.noreply.github.com>
2024-08-11 01:59:53 -0300

    minor fixes

--- Desafio 2/src/DatabaseTest.java
+++ Desafio 2/src/DatabaseTest.java
@@ -3,20 +3,19 @@ import java.sql.*;
 public class DatabaseTest {
     public static void main(String[] args) {
         Connection connection = null;
-        int period = 4;
         try {
             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/funcionarios","root", "");
 
             Statement statement = connection.createStatement();
 
             ResultSet resultSet = statement.executeQuery
-                    ("select e.`id`, e.`name`, sum(s.`value`)/"+ period +" as `average_salary`\n" +
+                    ("select e.`id`, e.`name`, avg(s.`value`) as `average_salary`\n" +
                             "from employee as e join salary as s\n" +
                             "on e.`id` = s.`employee_id`\n" +
-                            "where s.`payment_date` <= (CURRENT_DATE - "+ period * 30 +")\n" +
+                            "where datediff('2024-08-24', s.`payment_date`) < 92\n" +
                             "group by e.`id`\n" +
                             "order by `average_salary` desc\n" +
-                            "limit 3");
+                            "limit 3;");
 
             while (resultSet.next()) {
                 System.out.println(resultSet.getString("id") + " " +
